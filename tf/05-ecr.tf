ea726e79e84a1f7385b707651eced8eb60190e47
Jean-Gaël Choppe <jean-gael.choppe@epitech.eu>
2020-07-28 11:12:54 +0200

    terraform fmt

--- tf/05-ecr.tf
+++ tf/05-ecr.tf
@@ -6,24 +6,24 @@ data "aws_iam_role" "ecr" {
 }
 
 module "ecr" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-ecr.git?ref=master"
-  version                 = "0.21.0"
+  source  = "git::https://github.com/cloudposse/terraform-aws-ecr.git?ref=master"
+  version = "0.21.0"
 
-  namespace               = var.ecr_namespace
-  stage                   = var.ecr_stage
-  name                    = var.ecr_name
+  namespace = var.ecr_namespace
+  stage     = var.ecr_stage
+  name      = var.ecr_name
 
-  principals_full_access  = [data.aws_iam_role.ecr.arn]
+  principals_full_access     = [data.aws_iam_role.ecr.arn]
   principals_readonly_access = var.ecr_principals_readonly
 
-  enabled                 = var.ecr_enabled
-  attributes              = var.ecr_attributes
+  enabled    = var.ecr_enabled
+  attributes = var.ecr_attributes
 
   enable_lifecycle_policy = var.ecr_lifecycle_policy
 
-  image_names             = var.ecr_image_names
-  image_tag_mutability    = var.ecr_image_tag
-  max_image_count         = var.ecr_max_image
+  image_names          = var.ecr_image_names
+  image_tag_mutability = var.ecr_image_tag
+  max_image_count      = var.ecr_max_image
 
   #tags = {
   #  "name" = "ecr"
