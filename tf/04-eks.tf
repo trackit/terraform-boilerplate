194b97e67006ccc77af13ae3cb7c8286db567dc7
Bastien <bastien@Bastiens-MacBook-Pro.local>
2020-07-28 17:40:46 +0200

    fmt on eks

--- tf/04-eks.tf
+++ tf/04-eks.tf
@@ -19,14 +19,14 @@ provider "kubernetes" {
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "12.2.0"
-  cluster_name    = var.eks_cluster_name
-  cluster_version = "1.16"
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-  manage_aws_auth = true
-  config_output_path	  = "kubeconfig_${var.eks_cluster_name}-${var.env}"
+  source             = "terraform-aws-modules/eks/aws"
+  version            = "12.2.0"
+  cluster_name       = var.eks_cluster_name
+  cluster_version    = "1.16"
+  subnets            = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+  manage_aws_auth    = true
+  config_output_path = "kubeconfig_${var.eks_cluster_name}-${var.env}"
 
   worker_groups = [
     {
@@ -37,9 +37,9 @@ module "eks" {
   ]
 
   tags = {
-    Terraform          = "true"
-    Environnment       = var.env
-    TerraformWorkspace = terraform.workspace
+    Terraform           = "true"
+    Environnment        = var.env
+    TerraformWorkspace  = terraform.workspace
     propagate_at_launch = true
   }
 }
