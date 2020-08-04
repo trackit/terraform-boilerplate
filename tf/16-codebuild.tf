2a35929d00360212142779941136e4c514e3d389
Jules Klakosz <Jules Klakosz>
2020-08-04 16:36:09 +0200

    Finished Codebuild module

--- tf/16-codebuild.tf
+++ tf/16-codebuild.tf
@@ -1,23 +1,23 @@
+#
 # https://github.com/cloudposse/terraform-aws-codebuild
+#
 
 module "build" {
-  source                        = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=0.21.0"
-  namespace                     = var.codebuild_namespace
-  stage                         = var.env
-  name                          = var.codebuild_name
+  source                = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=0.21.0"
+  stage                 = var.env
+  name                  = var.codebuild_name
 
-  build_image                   = var.codebuild_image
-  build_compute_type            = var.codebuild_compute_type
-  build_timeout                 = var.codebuild_timeout
+  build_image           = var.codebuild_image
+  build_compute_type    = var.codebuild_compute_type
+  build_timeout         = var.codebuild_timeout
 
-  privileged_mode               = var.codebuild_privileged_mode
-  image_repo_name               = var.codebuild_target_repo_name
-  image_tag                     = var.codebuild_image_tag
+  privileged_mode       = var.codebuild_privileged_mode
 
-  artifact_type                 = var.codebuild_artifact_type
+  artifact_type         = var.codebuild_artifact_type
 
-  source_credential_server_type = var.codebuild_provider
-  github_token                  = var.github_token
+  github_token          = var.github_token
 
-  environment_variables         = var.codebuild_env_variables
+  environment_variables = var.codebuild_env_variables
+
+  tags                  = var.codebuild_tags
 }
