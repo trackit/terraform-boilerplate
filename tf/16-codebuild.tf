6afa98b1d39b65d263314ac75df14674d609b6d2
Jules Klakosz <Jules Klakosz>
2020-08-04 16:43:48 +0200

    Reformated file

--- tf/16-codebuild.tf
+++ tf/16-codebuild.tf
@@ -3,21 +3,21 @@
 #
 
 module "build" {
-  source = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=0.21.0"
-  stage = var.env
-  name = var.codebuild_name
+  source                = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=0.21.0"
+  stage                 = var.env
+  name                  = var.codebuild_name
 
-  build_image = var.codebuild_image
-  build_compute_type = var.codebuild_compute_type
-  build_timeout = var.codebuild_timeout
+  build_image           = var.codebuild_image
+  build_compute_type    = var.codebuild_compute_type
+  build_timeout         = var.codebuild_timeout
 
-  privileged_mode = var.codebuild_privileged_mode
+  privileged_mode       = var.codebuild_privileged_mode
 
-  artifact_type = var.codebuild_artifact_type
+  artifact_type         = var.codebuild_artifact_type
 
-  github_token = var.github_token
+  github_token          = var.github_token
 
   environment_variables = var.codebuild_env_variables
 
-  tags = var.codebuild_tags
+  tags                  = var.codebuild_tags
 }
