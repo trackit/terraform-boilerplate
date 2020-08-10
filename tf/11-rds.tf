3e234efc2869eb4fac4949386c81f85370897f86
Bastien <bastien@Bastiens-MacBook-Pro.local>
2020-08-10 14:23:45 +0200

    rds config updated

--- tf/11-rds.tf
+++ tf/11-rds.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "rds_security_group" {
   description = "Security group for RDS"
   vpc_id      = module.vpc.vpc_id
   ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    description = "RDS accessible from VPC"
+    from_port   = var.rds_port
+    to_port     = var.rds_port
     protocol    = "tcp"
     cidr_blocks = [module.vpc.vpc_cidr_block]
   }
@@ -16,7 +16,7 @@ resource "aws_security_group" "rds_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "allow_tls"
+    Name = "rds_security_group"
   }
 }
 
@@ -40,63 +40,24 @@ module "rds" {
 
   iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = aws_security_group.rds_security_group.id
+  vpc_security_group_ids = [aws_security_group.rds_security_group.id]
 
   maintenance_window = var.rds_maintenance
   backup_window      = var.rds_backup
 
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval    = var.rds_monitoring_interval
-  monitoring_role_arn    = var.rds_monitoring_role_arn
-  monitoring_role_name   = var.rds_monitoring_role_name
-  create_monitoring_role = true
+  tags = {
+    Terraform          = "true"
+    Environnment       = var.env
+    TerraformWorkspace = terraform.workspace
+  }
 
-  #tags = {
-  #  Owner       = "user"
-  #  Environment = "dev"
-  #}
-
-  # DB subnet group
   subnet_ids = module.vpc.database_subnets
 
-  # DB parameter group
   family = var.rds_family
 
-  # DB option group
   major_engine_version = var.rds_major_engine_version
 
-  # Snapshot name upon DB deletion
   final_snapshot_identifier = var.final_snapshot_identifier
 
-  # Database Deletion Protection
   deletion_protection = true
-
-  #parameters = [
-  #  {
-  #    name = "character_set_client"
-  #    value = "utf8"
-  #  },
-  #  {
-  #    name = "character_set_server"
-  #    value = "utf8"
-  #  }
-  #]
-
-  #options = [
-  #  {
-  #    option_name = "MARIADB_AUDIT_PLUGIN"
-  #
-  #    option_settings = [
-  #      {
-  #        name  = "SERVER_AUDIT_EVENTS"
-  #        value = "CONNECT"
-  #      },
-  #      {
-  #        name  = "SERVER_AUDIT_FILE_ROTATIONS"
-  #        value = "37"
-  #      },
-  #    ]
-  #  },
-  #]
 }
