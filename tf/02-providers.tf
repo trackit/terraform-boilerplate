b5cad8f9488c77e8e7d7b72e689f9ce7e5a78968
Jean-Gaël Choppe <jean-gael.choppe@epitech.eu>
2020-09-17 16:02:39 +0200

    tf fmt

--- tf/02-providers.tf
+++ tf/02-providers.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-boilerplate-qa"
-    key    = "terraform.state"
-    region = "us-east-1"
+    bucket         = "terraform-boilerplate-qa"
+    key            = "terraform.state"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
 }
