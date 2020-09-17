0046d8cc86383c05a1f7e6621f1dad6ae1fff9ca
Jean-Gaël Choppe <jean-gael.choppe@epitech.eu>
2020-09-17 15:51:23 +0200

    add terraform s3 backend

--- tf/02-providers.tf
+++ tf/02-providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-boilerplate-qa"
+    key    = "terraform.state"
+    region = "us-west-2"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
