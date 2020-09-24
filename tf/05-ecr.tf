a44645847dd7eac83a2841262c90167babeea36e
Jean-Gaël Choppe <jean-gael.choppe@epitech.eu>
2020-09-24 10:35:05 +0200

    JSON fmt

--- tf/05-ecr.tf
+++ tf/05-ecr.tf
@@ -3,8 +3,8 @@
 resource "aws_iam_role" "ecr" {
   name               = "test-role"
   assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
+{
+    "Version": "2012-10-17",
       "Statement": [
         {
           "Action": "sts:AssumeRole",
