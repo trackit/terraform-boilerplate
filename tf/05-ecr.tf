370dd7c3971331f212bccda2e71157c2081c2373
Jean-Gaël Choppe <jean-gael.choppe@epitech.eu>
2020-10-01 12:18:25 +0200

    feat(ecr): commented line on principals full access var & removed aws iam role resource

--- tf/05-ecr.tf
+++ tf/05-ecr.tf
@@ -1,48 +1,5 @@
 # https://github.com/cloudposse/terraform-aws-ecr
 
-resource "aws_iam_role" "ecr" {
-  name               = "test-role"
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "ec2.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    }
-EOF
-}
-
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "attach" {
-  role       = aws_iam_role.ecr.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-
 module "ecr" {
   source = "git::https://github.com/cloudposse/terraform-aws-ecr.git?ref=0.21.0"
 
@@ -50,7 +7,7 @@ module "ecr" {
   stage     = var.ecr_stage
   name      = var.ecr_name
 
-  principals_full_access     = [aws_iam_role.ecr.arn]
+  //principals_full_access     = [aws_iam_role.ecr.arn]
   principals_readonly_access = var.ecr_principals_readonly
 
   enabled    = var.ecr_enabled
