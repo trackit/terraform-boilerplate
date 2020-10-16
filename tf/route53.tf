9ed1719fa6e268fed2673c7e81b5855fd65666e3
Jean-Gaël Choppe <jean-gael.choppe@epitech.eu>
2020-10-16 17:37:19 +0200

    feat(tfvars): remove route53 vars and put default value

--- tf/route53.tf
+++ tf/route53.tf
@@ -10,10 +10,10 @@ resource "aws_route53_zone" "primary_zone" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
 
 resource "aws_route53_record" "record" {
-  zone_id = aws_route53_zone.private.zone_id
+  zone_id = aws_route53_zone.primary_zone.zone_id
   name    = "www.example.com"
-  type    = var.route53_type
-  ttl     = var.route53_ttl
-  records = var.route53_records
+  type    = A
+  ttl     = 300
+  records = null
 }
 */
