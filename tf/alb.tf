ebe664db4a3c4447b6fa014734b7797d39a5f42e
jgchoppe <jean-gael.choppe@epitech.eu>
2020-10-23 16:11:53 +0200

    feat(alb): add acm cert

--- tf/alb.tf
+++ tf/alb.tf
@@ -36,6 +36,7 @@ module "alb" {
     {
       port               = 443
       protocol           = "HTTPS"
+      certificate_arn    = aws_acm_certificate.cert.arn
       target_group_index = 0
     }
   ]
@@ -52,4 +53,32 @@ module "alb" {
   ]
 
   tags = local.tags
+}
+
+resource "aws_acm_certificate" "cert" {
+  private_key      = tls_private_key.example_key.private_key_pem
+  certificate_body = tls_self_signed_cert.example_cert.cert_pem
+}
+
+resource "tls_private_key" "example_key" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+resource "tls_self_signed_cert" "example_cert" {
+  key_algorithm   = "ECDSA"
+  private_key_pem = tls_private_key.example_key.private_key_pem
+
+  subject {
+    common_name  = "example.com"
+    organization = "ACME Examples, Inc"
+  }
+
+  validity_period_hours = 12
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
 }
