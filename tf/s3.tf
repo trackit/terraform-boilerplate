4afd8ecfcd07d84a5faf621af70e56dd83ce45ce
jgchoppe <jean-gael.choppe@epitech.eu>
2021-01-08 15:03:25 +0100

    feat(ecs): comment policy document

--- tf/s3.tf
+++ tf/s3.tf
@@ -55,8 +55,8 @@ module "s3" {
   tags = local.tags
 }
 
-data "aws_iam_policy_document" "s3" {
-}
+#data "aws_iam_policy_document" "s3" {
+#}
 
 ##### S3 Objects
 ## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
