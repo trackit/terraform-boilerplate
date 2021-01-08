76b1191f0cdb4ff1585f37bdbf082307080d1ccd
jgchoppe <jean-gael.choppe@epitech.eu>
2021-01-08 16:29:27 +0100

    feat(ecs): comment s3 lambda on file upload

--- tf/s3.tf
+++ tf/s3.tf
@@ -84,13 +84,13 @@ resource "aws_s3_bucket_object" "file" {
 ## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_notification
 
 ## Lambda on file upload
-resource "aws_s3_bucket_notification" "s3_lambda_on_upload" {
-  bucket = module.s3.this_s3_bucket_id
-
-  lambda_function {
-    lambda_function_arn = module.lambda_function.this_lambda_function_arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "input/"
-    filter_suffix       = ".zip"
-  }
-}
+#resource "aws_s3_bucket_notification" "s3_lambda_on_upload" {
+#  bucket = module.s3.this_s3_bucket_id
+#
+#  lambda_function {
+#    lambda_function_arn = module.lambda_function.this_lambda_function_arn
+#    events              = ["s3:ObjectCreated:*"]
+#    filter_prefix       = "input/"
+#    filter_suffix       = ".zip"
+#  }
+#}
