93d160e935f59f8483e2572976105b2fc96beedd
jgchoppe <jean-gael.choppe@epitech.eu>
2021-01-29 12:20:14 +0100

    fix(cloudfront): changes on resources order

--- tf/cloudfront.tf
+++ tf/cloudfront.tf
@@ -1,5 +1,15 @@
 # https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn/tree/0.44.0
 
+
+resource "aws_s3_bucket" "b" {
+  bucket = "terraform-boilerplate-cdn"
+
+  acl           = "private"
+  force_destroy = true
+
+  tags = local.tags
+}
+
 module "cdn" {
   source  = "cloudposse/cloudfront-s3-cdn/aws"
   version = "0.44.0"
@@ -9,15 +19,6 @@ module "cdn" {
   parent_zone_id       = aws_s3_bucket.b.hosted_zone_id
   origin_bucket        = aws_s3_bucket.b.bucket
   origin_force_destroy = true
-
-  tags = local.tags
-}
-
-resource "aws_s3_bucket" "b" {
-  bucket = "terraform-boilerplate-cdn"
-
-  acl           = "private"
-  force_destroy = true
 
   tags = local.tags
 }
