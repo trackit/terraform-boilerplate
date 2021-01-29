b6096e7530f47cd17dc3d3bd2ca7b21091c447f1
jgchoppe <jean-gael.choppe@epitech.eu>
2021-01-29 12:14:51 +0100

    feat(cloudfront): cloudfront module testing

--- tf/cloudfront.tf
+++ tf/cloudfront.tf
@@ -1,89 +1,27 @@
-# https://registry.terraform.io/modules/terraform-aws-modules/cloudfront/aws/1.1.0
+# https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn/tree/0.44.0
 
 module "cdn" {
-  source  = "terraform-aws-modules/cloudfront/aws"
-  version = "1.1.0"
+  source  = "cloudposse/cloudfront-s3-cdn/aws"
+  version = "0.44.0"
 
-  aliases = var.cdn_aliases
-
-  comment             = var.cdn_comment
-  enabled             = var.cdn_enabled
-  is_ipv6_enabled     = var.cdn_is_ipv6_enabled
-  price_class         = var.cdn_price_class
-  retain_on_delete    = var.cdn_retain_on_delete
-  wait_for_deployment = var.cdn_wait_for_deployment
-
-  create_origin_access_identity = var.cdn_create_origin_access_identity
-  origin_access_identities = {
-    s3_bucket_one = "My awesome CloudFront can access"
-  }
-
-  logging_config = {}
-
-  ## One or more origins for this distribution (multiples allowed).
-  origin = {
-    something = {
-      domain_name = module.s3.this_s3_bucket_bucket_regional_domain_name
-      custom_origin_config = {
-        http_port              = 80
-        https_port             = 443
-        origin_protocol_policy = "match-viewer"
-        origin_ssl_protocols = [
-          "TLSv1"
-        ]
-      }
-    }
-  }
-
-  ## The map of cache behaviors for this distribution.
-  ## Key default will be used as the default cache behavior, all other keys will be used as ordered list of cache behaviors.
-  ## List from top to bottom in order of precedence.
-  ## The topmost cache behavior will have precedence 0.
-  cache_behavior = {
-    default = {
-      target_origin_id       = "something"
-      viewer_protocol_policy = "allow-all"
-
-      allowed_methods = ["GET", "HEAD", "OPTIONS"]
-      cached_methods  = ["GET", "HEAD"]
-      compress        = true
-      query_string    = true
-    }
-
-    s3 = {
-      path_pattern           = "/static/*"
-      target_origin_id       = "s3_one"
-      viewer_protocol_policy = "redirect-to-https"
-
-      allowed_methods = ["GET", "HEAD", "OPTIONS"]
-      cached_methods  = ["GET", "HEAD"]
-      compress        = true
-      query_string    = true
-    }
-  }
-
-  viewer_certificate = {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
-    ssl_support_method  = "sni-only"
-  }
+  name = "app"
+  #aliases          = ["assets.cloudposse.com"]
+  parent_zone_id       = aws_s3_bucket.b.hosted_zone_id
+  origin_bucket        = aws_s3_bucket.b.bucket
+  origin_force_destroy = true
 
   tags = local.tags
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = var.cdn_acm_domain_name
-  validation_method = var.cdn_acm_method
+resource "aws_s3_bucket" "b" {
+  bucket = "terraform-boilerplate-cdn"
 
-  tags = local.tags
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-module "s3_one" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket        = "cdn-terraform-boilerplate"
+  acl           = "private"
   force_destroy = true
+
+  tags = local.tags
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
 }
